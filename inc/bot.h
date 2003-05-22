886e096e60188b833f1e5c2370e67f8d31202c55
japa <japa@d4333498-d51f-0410-a6fe-c081ca229757>
2003-05-22 20:44:31 +0000

    Initial revision
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@463 d4333498-d51f-0410-a6fe-c081ca229757

---
+++ inc/bot.h
@@ -0,0 +1,27 @@
+/** @file bot.h
+ * Definition of Dippaadai.
+ * Rairairai.
+ *
+ * @todo foo
+ *
+ * @author Japa
+ * @version 0.1
+ */
+
+#ifndef BOT_H
+#define BOT_H
+
+/**
+ * Plop. @see Bar
+ */
+class CBot
+{
+public:
+	/**
+	 * Default constructor.
+	 */
+	CBot();
+}
+
+
+#endif // BOT_H
