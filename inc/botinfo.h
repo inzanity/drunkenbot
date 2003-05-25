36fbbef2af35b0aea66720ed4515cc10c504d077
inzane <inzane@d4333498-d51f-0410-a6fe-c081ca229757>
2003-05-25 20:39:32 +0000

    Now a working file.
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@477 d4333498-d51f-0410-a6fe-c081ca229757

--- inc/botinfo.h
+++ inc/botinfo.h
@@ -13,21 +13,33 @@
 #ifndef BOTINFO_H
 #define BOTINFO_H
 
-/** Public class for general bot information
+/**
+ * Public class for general bot information
  * When bots see other bots, they will get this information of it.
  */
-class CVisibleBotInfo : CMovingGameObj
+class CVisibleBotInfo : public CMovingGameObj
 {
 public:
 	/** Default constructor */
 	CVisibleBotInfo();
-	/** Copy constructor
+	/**
+	 * Copy constructor
 	 * Will be used when giving info to bots, changes coordinates from
 	 * absolute to relative
 	 *
 	 * @param aBotInfo The private info of the bot
-	 * @param aXPos Curren X coordinate of the bot
-	 * @param :
+	 * @param aXPos Current X coordinate of the bot
+	 * @param aYPos Current Y coordinate of the bot
+	 */
+	CVisibleBotInfo(CVisibleBotInfo *aBotInfo, float aXPos, float aYPos);
+};
 
 /**
- * 
+ * Private class for general bot information
+ */
+class CBotInfo : public CVisibleBotInfo
+{
+public:
+	/** Default constructor */
+	CBotInfo();
+};
