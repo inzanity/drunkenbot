3716abb03bffe72ed4badbac96ebfabab74bf4cd
japa <japa@d4333498-d51f-0410-a6fe-c081ca229757>
2003-05-29 14:16:28 +0000

    First versions of CGameObj and CMovingGameObj.
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@482 d4333498-d51f-0410-a6fe-c081ca229757

--- inc/gameobj.h
+++ inc/gameobj.h
@@ -11,7 +11,7 @@
 #ifndef GAMEOBJ_H
 #define GAMEOBJ_H
 
-class CVisibleBotInfo;
+class CBotInfo;
 
 /**
  * Base class for all game objects.
@@ -20,30 +20,76 @@ class CVisibleBotInfo;
 class CGameObj
 {
 public:
-	/** Constructor. @param aType Type of the game object. @todo Game object type explanation. */
+	/** Constructor. @param aType Type of the game object. @see mType. */
 	CGameObj(int aType);
 
 	/** Destructor. */
 	virtual ~CGameObj();
 
-	/** Setter for object position. @param aXPos Position */
+	/** Setter for the position of the object. @param aXPos X-coordinates. @param aYPos Y-coordinates. */
 	void setPos(float aXPos, float aYPos);
 
+	/** Getter for x-coordinates of the object. */
 	float xPos();
 	
+	/** Getter for y-coordinates of the object. */
 	float yPos();
 
-private:
-	CBotAI *mBotAI;
-	CBotInfo mBotInfo;
+	/** Getter for the radius. Used with drawing and collision detection. */
+	float radius();
+
+	/** Getter for the orientation of the object. */
+	float angle();
+
+	/** Getter for the type of the object. @see mType. */
+	int type();
+
+protected:
+	/** X-coordinates of the object. */
+	float mXPos;
+	/** Y-coordinates of the object. */
+	float mYPos;
+	/** Radius of the game object. */
+	float mRadius;
+	/** Orientation of the game object. With moving objects, angle is used as a moving direction. */
+	float mAngle;
+	/** Type of the game object. @todo Game object type specification. */
+	int mType;
 };
 
+/**
+ * Base class for all moving game objects.
+ * Includes speed and collision detection.
+ */
 class CMovingGameObj : public CGameObj
 {
 public:
-	CMovingGameObj();
+	/** Constructor. @param aType Type of the object. */
+	CMovingGameObj(int aType);
+
+	/** Destructor. */
 	virtual ~CMovingGameObj();
+
+	/** Setter for the speed. */
+	void setSpeed(float aVelocity, float aAngle);
+
+	/** Getter for the velocity. */
+	float velocity();
+
+	/**
+	 * Moves object according to the speed. In a case of collision, both objects are stopped and
+	 * affected by the collision.
+	 * @param aTileMap Tilemap to detect collisions. Bots can collide with walls and obstacles,
+	 * while bullets can collade only with walls.
+	 * @param aBots List of all bots.
+	 * @return true if moving was succesful, false in a case of collision.
+	 */
+	bool move(char **aTileMap, CBotInfo *aBots);
+
+protected:
+	/** Velocity of the moving game object. Moving direction depends on the orientation of the object. */
+	float mVelocity;
 };
 
 
-#endif // BOT_H
+#endif // GAMEOBJ_H
