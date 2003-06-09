2ae4e2f7c4b39dcad5a8f9605adda258c0b11b74
japa <japa@d4333498-d51f-0410-a6fe-c081ca229757>
2003-06-09 22:44:49 +0000

    First implementation of CBot.
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@517 d4333498-d51f-0410-a6fe-c081ca229757

---
+++ src/bot.cpp
@@ -0,0 +1,99 @@
+#include <windows.h>
+#include <cstring>
+#include <cstdlib>
+#include "../inc/bot.h"
+
+	CBotAI *mBotAI;
+	char *mDllName;
+	HMODULE mDllHandle;
+	int mFrags;
+
+CBot::CBot(const char *aDllName) : mBotAI(NULL), mDllName(NULL), mDllHandle(NULL), mFrags(0)
+{
+	mDllName = new char[strlen(aDllName) + 1];
+	strcpy(mDllName, aDllName);
+}
+
+void CBot::spawn(const char ** /*aTilemap*/, int aWidth, int aHeight, const CGameObj * /*aGameObjects*/)
+{
+	mPos.mX = float(rand() % aWidth);
+	mPos.mY = float(rand() % aHeight);
+	loadAI();
+}
+
+void CBot::think(const char ** /*aTilemap*/, CVisibleBotInfo * /*aBots*/, list<CBulletInfo *> * /*aBulletList*/,
+				 list<CWeaponInfo *> * /*aWeaponList*/, list<TPosition> * /*aVoices*/)
+{
+	if (!mBotAI)
+		return;
+	// TODO: copy data and update visible objects
+	mBotAI->think();
+}
+
+void CBot::performActions(list<CWeaponInfo *> * /*aBulletList*/, list<TPosition> * /*aVoices*/)
+{
+	if (!mBotAI)
+		return;
+	if (mActionDelay)
+		if (--mActionDelay == 0)
+		{
+			if (mBotAction == EActionBunker)
+				mBunkered = !mBunkered;
+			mBotAction = EActionNone;
+		}
+
+	if (!mBotAI->checkAction() ||
+		(mActionDelay && mBotAI->action() > EActionMove) ||
+		(mBunkered && (mBotAI->action() & EActionMove)))
+		mBotAI->resetAction();
+	mBotAction = (TBotAction)(mBotAI->action() & (EActionShoot | EActionBunker | EActionPickWeapon | EActionDropWeapon));
+	if (mBotAction == EActionShoot && mWeapon->shoot())
+	{
+		// TODO: Add bullet and voice.
+		mActionDelay = (char)mWeapon->reloadTime();
+	}
+	else if (mBotAction == EActionBunker)
+		mActionDelay = 4;
+	else if (mBotAction == EActionPickWeapon)
+		mActionDelay = 2;
+	else if (mBotAction == EActionDropWeapon)
+		mActionDelay = 1;
+}
+
+void CBot::chkCollision(const char ** /*aTilemap*/, CBotInfo ** /*aBots*/)
+{
+	// TODO
+	if (!mBotAI)
+		return;
+}
+
+void CBot::update(float aTimeFactor)
+{
+	if (!mBotAI)
+		return;
+	move(aTimeFactor);
+	// TODO: Set mAnimationTimer.
+}
+
+void CBot::loadAI()
+{
+	if (mBotAI)
+		delete mBotAI;
+
+#ifdef _WINDOWS_
+	if (mDllHandle)
+		FreeLibrary(mDllHandle);
+	mDllHandle = LoadLibrary(mDllName);
+	if (mDllHandle)
+	{
+		TBotAIGetter getter = (TBotAIGetter)GetProcAddress(mDllHandle, "getBotAI");
+		if (getter)
+			mBotAI = getter();
+//		else
+//			cout<<"No function found\n";
+	}
+//	else
+//		cout<<"No library found.\n";
+#endif
+	// TODO: #ifdef _PURKKA_
+}
