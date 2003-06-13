408ed60456049e2fe2020b0fc905ef99ce55303e
inzane <inzane@d4333498-d51f-0410-a6fe-c081ca229757>
2003-06-13 02:48:39 +0000

    Minor fix, killed two warnings and fixed ANSI escape sequence.
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@552 d4333498-d51f-0410-a6fe-c081ca229757

--- src/textengine.cpp
+++ src/textengine.cpp
@@ -37,7 +37,7 @@ void CTextEngine::drawTilemap(char **aTilemap, int aWidth, int aHeight)
 void CTextEngine::drawGameObj(const CGameObj *aGameObj)
 {
 	int x, y;
-	x = floor(aGameObj->xPos());
-	y = floor(aGameObj->yPos());
-	printf("\033[%d:%dH%c", y, x, mBotSymbols[(aGameObj->type() >> 4) & 3]);
+	x = (int)floor(aGameObj->xPos());
+	y = (int)floor(aGameObj->yPos());
+	printf("\033[%d;%dH%c", y, x, mBotSymbols[(aGameObj->type() >> 4) & 3]);
 }
