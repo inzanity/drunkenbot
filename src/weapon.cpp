29183ca7d0695cebbe39c7df56557cbd6230913f
inzane <inzane@d4333498-d51f-0410-a6fe-c081ca229757>
2003-06-17 23:10:27 +0000

    Reordered initializing list to match order in class definition.
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@619 d4333498-d51f-0410-a6fe-c081ca229757

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -5,7 +5,7 @@
 
 using std::istream;
 
-CWeapon::CWeapon(istream *aStream) : mBulletCount(0), mBulletSpeed(0.f), mExplosionRadius(0.f), mExplosionSpeed(0.f), mDamage(0.f)
+CWeapon::CWeapon(istream *aStream) : mReloadTime(0), mBulletCount(0), mBulletSpeed(0.f), mExplosionRadius(0.f), mExplosionSpeed(0.f), mDamage(0.f)
 {
 	*aStream >> mBulletCount
 		>> mReloadTime
@@ -18,7 +18,7 @@ CWeapon::CWeapon(istream *aStream) : mBulletCount(0), mBulletSpeed(0.f), mExplos
 		mDamage /= ceilf(mExplosionRadius / mExplosionSpeed);
 }
 
-CWeapon::CWeapon(const CWeapon *aWeapon) : mBulletCount(aWeapon->mBulletCount), mBulletSpeed(aWeapon->mBulletSpeed), mExplosionRadius(aWeapon->mExplosionRadius), mExplosionSpeed(aWeapon->mExplosionSpeed), mDamage(aWeapon->mDamage), mReloadTime(aWeapon->mReloadTime)
+CWeapon::CWeapon(const CWeapon *aWeapon) : mReloadTime(aWeapon->mReloadTime), mBulletCount(aWeapon->mBulletCount), mBulletSpeed(aWeapon->mBulletSpeed), mExplosionRadius(aWeapon->mExplosionRadius), mExplosionSpeed(aWeapon->mExplosionSpeed), mDamage(aWeapon->mDamage)
 {
 }
 
