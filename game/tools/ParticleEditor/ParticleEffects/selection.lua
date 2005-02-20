295dfb25fdbf1f0e137589fd150a5aaa1edee10c
japa <japa@d4333498-d51f-0410-a6fe-c081ca229757>
2005-02-20 18:08:44 +0000

    optimized selction.lua
    git-svn-id: file:///home/inzane/.dbot-svn-fix/trunk@334 d4333498-d51f-0410-a6fe-c081ca229757

--- game/tools/ParticleEditor/ParticleEffects/selection.lua
+++ game/tools/ParticleEditor/ParticleEffects/selection.lua
@@ -10,13 +10,13 @@ initParticleSystem(Options)
 size = 1
 life = 2000
 pos = {}
+for j = 1, Options.Coordinates do
+	pos[j] = {}
+	pos[j].x = math.cos(2*3.1415*(j - 1)/(Options.Coordinates - 1)) * size
+	pos[j].y = 0
+	pos[j].z = math.sin(2*3.1415*(j - 1)/(Options.Coordinates - 1)) * size
+end
 for i = 1, Options.Particles do
-	for j = 1, Options.Coordinates do
-		pos[j] = {}
-		pos[j].x = math.cos(2*3.1415*(j - 1)/(Options.Coordinates - 1)) * size
-		pos[j].y = 0
-		pos[j].z = math.sin(2*3.1415*(j - 1)/(Options.Coordinates - 1)) * size
-	end
 	f = math.mod(4 * (i - 1) / Options.Particles, 1)
 	color = {rgb(50*f, 50*f, 255*f)}
 	setParticle(i, life, i/Options.Particles*life, pos, color)
